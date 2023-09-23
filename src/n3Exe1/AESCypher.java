e8c0b84df3b22b2c29e2a9886911a4ceed512365
pedrolopez <pedrolopez@10.201.1.61>
2023-09-23 12:23:30 +0200

    develops AESCypher Class based on model and requirement guidelines

--- src/n3Exe1/AESCypher.java
+++ src/n3Exe1/AESCypher.java
@@ -1,10 +1,16 @@
 package n3Exe1;
 
 import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.Base64;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
 public class AESCypher {
@@ -22,4 +28,37 @@ public class AESCypher {
 		
 		return secretKey;
 	}
+	
+	public String encrypt (String data, String key) throws UnsupportedEncodingException, NoSuchAlgorithmException, 
+													InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, 
+														BadPaddingException {
+		SecretKeySpec secretKey = this.createKey(key);
+		
+		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+		cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+		
+		byte[] dataToEncrypt = data.getBytes("UTF-8");
+		byte[] encryptedBytes = cipher.doFinal(dataToEncrypt);
+		String encrypted = Base64.getEncoder().encodeToString(encryptedBytes);
+		
+		
+		return encrypted;
+	}
+	
+	public String desencrypt (String encryptedData, String key) throws UnsupportedEncodingException, NoSuchAlgorithmException, 
+															InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, 
+																BadPaddingException {
+		SecretKeySpec secretKey = this.createKey(key);
+		
+		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+		cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+		
+		byte[] encryptedBytes = Base64.getDecoder().decode(encryptedData);
+		byte[] dataDesencrypted = cipher.doFinal(encryptedBytes);
+		String data = new String(dataDesencrypted);
+		
+		return data;
+	}
+	
+	
 }
