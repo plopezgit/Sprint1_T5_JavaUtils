98f9a2d8c7f1ab52e1a774378b538c124b3aca47
pedrolopez <pedrolopez@10.201.1.61>
2023-09-25 15:51:00 +0200

    updates DirectoryAlphabeticList for n3Exe1 package by adopting latest exercise improvements; test passed

--- src/n3Exe1/App.java
+++ src/n3Exe1/App.java
@@ -1,71 +1,32 @@
 package n3Exe1;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.nio.file.*;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Properties;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 
 public class App {
 
 	public static void main(String[] args) {
-
-		Properties properties = new Properties();
-		AESCypher encrypter = new AESCypher();
-		FileOutputStream fileOutputStream;
-		ObjectOutputStream objectOutputStream;
-		FileInputStream fileInputStream;
-		ObjectInputStream objectInputStream;
-		DirectoryAlphabeticList dir2 = new DirectoryAlphabeticList();
-
-		try {
-			properties.load(new FileReader("file.properties"));
-		} catch (FileNotFoundException e1) {
-			System.err.println("File not found.");
-		} catch (IOException e1) {
-			e1.printStackTrace();
+		
+		DirectoryAlphabeticList directoryAlphaList = new DirectoryAlphabeticList(new File("testDir"));
+		for (String s : directoryAlphaList.getOrderedFileTreeRecursively()) {
+			directoryAlphaList.saveDirectoryBackupToFile(s);
 		}
-
+		
 		try {
-			fileOutputStream = new FileOutputStream(properties.getProperty("file"));
-			objectOutputStream = new ObjectOutputStream(fileOutputStream);
-			Path path2 = Paths.get(properties.getProperty("directoryRead"));
-
-
-			String encryptedDirAlphaList = encrypter.encrypt(dir2.getFileTreeFrom(path2).toString(), properties.getProperty("encryptionKey"));
-
-			objectOutputStream.writeObject(encryptedDirAlphaList);
-			objectOutputStream.close();
-
-		} catch (IOException | InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException
-				| IllegalBlockSizeException | BadPaddingException e) {
-			e.printStackTrace();
+			directoryAlphaList.readDirectoryFromBackup();
+		} catch (IOException e) {
+			e.getMessage();
 		}
-
+		
 		try {
-			fileInputStream = new FileInputStream(properties.getProperty("file"));
-			objectInputStream = new ObjectInputStream(fileInputStream);
-
-			String desencryptedDirAlphaList = encrypter.desencrypt(objectInputStream.readObject().toString(),
-					properties.getProperty("encryptionKey"));
-			
-			
-			
-			System.out.println(desencryptedDirAlphaList);
-			objectInputStream.close();
-
-		} catch (IOException | ClassNotFoundException | InvalidKeyException | NoSuchAlgorithmException
-				| NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException e) {
+			directoryAlphaList.serializeDirectoryToFile();
+		} catch (IOException e) {
+			e.getMessage();
+		}
+		
+		try {
+			directoryAlphaList.desSeriaizeDirectoryFromFileToObject();
+		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 
--- src/n3Exe1/DirectoryAlphabeticList.java
+++ src/n3Exe1/DirectoryAlphabeticList.java
@@ -1,81 +1,160 @@
 package n3Exe1;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.nio.file.DirectoryStream;
-import java.nio.file.FileSystems;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.time.LocalDate;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
 
-public class DirectoryAlphabeticList implements Comparator<Path>, Serializable {
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+public class DirectoryAlphabeticList implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	private ArrayList<String> directory;
+	private Properties properties;
+	AESCypher encrypter;
+	private File dir;
+	private ArrayList<String> directoryList;
+	private final String FILE_NOT_FOUND_MSG = "File not found";
 
-	public DirectoryAlphabeticList() {
-		directory = new ArrayList<String>();
+	public DirectoryAlphabeticList(File dir) {
+		properties = new Properties();
+		encrypter = new AESCypher();
+		directoryList = new ArrayList<String>();
+		this.dir = dir;
 	}
 	
-	public ArrayList<String> getDirectory() {
-		return directory;
+	public ArrayList<String> getDirectoryList() {
+		return directoryList;
 	}
-	
-	public ArrayList<String> getFileTreeFrom (Path dir) {
-		try {
-			Files.walkFileTree(dir, new SimpleFileVisitor<Path>(){
-				  @Override
-				  public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-				    if(!attrs.isDirectory()){
-				       directory.add("(D) " + file.getParent() + "\n(F) " + file.getFileName() + " | Modified: " + attrs.lastModifiedTime());
-				    }
-				    return FileVisitResult.CONTINUE;
-				  }
-				  });
-		} catch (IOException e) {
-			e.printStackTrace();
+
+	public void setDirectoryList(ArrayList<String> directoryList) {
+		this.directoryList = directoryList;
+	}
+
+
+
+	private ArrayList<String> goThroughDirectoryTree(File dir, String[] directory) {
+		Collections.sort(Arrays.asList(directory));
+		for (String item : directory) {
+			File file = new File(dir.getAbsolutePath(), item);
+			if (file.isDirectory()) {
+				directoryList.add("(D) " + file.getName() + " | Modified: " + simpleDateFormat(file.lastModified()));
+				goThroughDirectoryTree(file, file.list());
+			} else {
+				directoryList.add("(F) " + file.getName() + " | Modified: " + simpleDateFormat(file.lastModified()));
+			}
 		}
 		
-		directory.sort(Comparator.naturalOrder());
-
-		return directory;
+		return directoryList;
 	}
 	
-	public void saveDirectoryBackup(String path) {
+	public ArrayList<String> getOrderedFileTreeRecursively() {
+		String[] directory = this.dir.list();
+		return goThroughDirectoryTree(dir, directory);
+	}
+	
+	private String simpleDateFormat (long date) {
+		DateFormat dateFormat =  new SimpleDateFormat("dd-MM-yyyy hh-MM-ss");
+		String lastModifiedDateFormatted = dateFormat.format(date);
+		return lastModifiedDateFormatted;
+	}
+	
+	public void saveDirectoryBackupToFile (String path) {
 
 		try {
-			FileWriter output = new FileWriter("directoryBackup.txt", true);
+			loadDirectoryAlphabeticListProperties();
+			FileWriter output = new FileWriter(properties.getProperty("fileTxtPath"), true);
 			BufferedWriter buffer = new BufferedWriter(output);
-			buffer.write(path);
+			buffer.write(path + "\n");
 			buffer.close();
 		} catch (IOException event) {
-			System.out.println("File not found.");
+			System.err.println(FILE_NOT_FOUND_MSG);
 		}
 	}
+	
+	public void readDirectoryFromBackup () throws IOException {
+		try {
+			loadDirectoryAlphabeticListProperties();
+			FileReader input = new FileReader 
+					(properties.getProperty("fileTxtPath"));
+			BufferedReader buffer = new BufferedReader(input);
+			String line = "";
+			while (line != null) {
+				line = buffer.readLine();
+				System.out.print(line + "\n");
+			}
+			buffer.close();
+		} catch (IOException event) {
+			System.err.println(FILE_NOT_FOUND_MSG);
+		}
+	}
+	
+	public void serializeDirectoryToFile () throws IOException {
+		try {
+			loadDirectoryAlphabeticListProperties();
+			FileOutputStream fileOutputStream = new FileOutputStream(properties.getProperty("fileSerPath"));
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+			String encryptedDirAlphaList = encrypter.encrypt(this.toString(), properties.getProperty("encryptionKey"));
 
-	@Override
-	public int compare(Path dir1, Path dir2) {
-		Path dir1NameToCompare = dir1.getFileName();
-		Path dir2NameToCompare = dir2.getFileName();
-		return dir2NameToCompare.compareTo(dir1NameToCompare);
+			objectOutputStream.writeObject(encryptedDirAlphaList);
+			objectOutputStream.close();
+		} catch (IOException | InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException e) {
+			System.err.println(FILE_NOT_FOUND_MSG);
+		}
+	}
+	
+	public void desSeriaizeDirectoryFromFileToObject () throws IOException, ClassNotFoundException {
+		try {
+			loadDirectoryAlphabeticListProperties();
+			FileInputStream fileOutputStream = new FileInputStream(properties.getProperty("fileSerPath"));
+			ObjectInputStream objectInputStream = new ObjectInputStream(fileOutputStream);
+			String desencryptedDirAlphaList = encrypter.desencrypt(objectInputStream.readObject().toString(),
+					properties.getProperty("encryptionKey"));
+			objectInputStream.close();
+			System.out.println(desencryptedDirAlphaList);
+			
+		} catch (IOException | ClassNotFoundException | InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException e) {
+			System.err.println(FILE_NOT_FOUND_MSG);
+		}
+	}
+	
+	private void loadDirectoryAlphabeticListProperties () throws IOException {
+		try {
+			this.properties.load(new FileReader("file.properties"));
+		} catch (IOException e) {
+			System.err.println(FILE_NOT_FOUND_MSG);
+		}
 	}
 
 	@Override
 	public String toString() {
-		return "DirectoryAlphabeticList: \n" + directory;
+		return "Directory Alphabetic List: " + directoryList;
 	}
 	
+	
+	
 	/*
-	 * Ahora el programa debe serializar un Objeto Java a un archivo 
-	 * .ser y después debe desserializarlo.
+	 * Crea una utilidad que encripte y desencripte 
+	 * los archivos resultantes de los niveles anteriores.
+	 * Utiliza el algoritmo AES en modo de trabajo ECB o CBC 
+	 * con método de llenado PKCS5Padding. Se puede utilizar 
+	 * javax.crypto o bien org.apache.commons.crypto.
 	 */
 }
