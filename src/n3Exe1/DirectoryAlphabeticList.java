ac2a62e5179c06d86b9847147906f2b580405346
pedrolopez <p.lopez.ch.hr@hotmail.com>
2023-09-27 17:25:00 +0200

    moves autocloseable resources within try-with-resources statement Signed-off-by: pedrolopez <p.lopez.ch.hr@hotmail.com>

--- src/n3Exe1/DirectoryAlphabeticList.java
+++ src/n3Exe1/DirectoryAlphabeticList.java
@@ -35,10 +35,10 @@ public class DirectoryAlphabeticList implements Serializable {
 
 	public DirectoryAlphabeticList(File dir) {
 		properties = new Properties();
+		loadDirectoryAlphabeticListProperties();
 		encrypter = new AESCypher();
 		directoryList = new ArrayList<String>();
 		this.dir = dir;
-		loadDirectoryAlphabeticListProperties();
 	}
 
 	public ArrayList<String> getDirectoryList() {
@@ -124,8 +124,8 @@ public class DirectoryAlphabeticList implements Serializable {
 	}
 
 	private void loadDirectoryAlphabeticListProperties() {
-		try {
-			this.properties.load(new FileReader("file.properties"));
+		try (FileReader reader = new FileReader("file.properties")){
+			properties.load(reader);
 		} catch (IOException e) {
 			System.err.println(FILE_NOT_FOUND_MSG);
 		}
